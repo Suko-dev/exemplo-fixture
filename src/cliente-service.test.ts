e85036d74c078e24f5b117eb71c7a808828560b3
Marcos Ribeiro <mvribeirodev@google.com>
2024-03-11 22:27:20 -0300

    feat: builder de cliente e adição do telefone

--- src/cliente-service.test.ts
+++ src/cliente-service.test.ts
@@ -1,76 +1,69 @@
 import { ClienteService } from "./cliente-service";
 import { validate, version } from 'uuid'
+import { ClienteDtoBuilder } from './cliente-builder'
 
 describe('ClienteService', () => {
     let clienteService: ClienteService
     let salvarCliente
+    let builder: ClienteDtoBuilder
 
     beforeEach(() => {
-        salvarCliente = jest.fn()
+        // erro estava aqui. eu coloquei um once e o teste do repositório estava com o only, daí estava resolvendo a promise e quebrando a toda a suite pois só ele estava rodando
+        salvarCliente = jest.fn().mockResolvedValue(true)
+
         const clienteRepository = { salvar: salvarCliente }
         clienteService = new ClienteService(clienteRepository)
+        builder = new ClienteDtoBuilder()
+    })
+
+    describe('Salvar no repositório', () => {
+        test('Deve lançar uma excessão quando o repositório não conseguir salvar o cliente', () => {
+            salvarCliente.mockResolvedValueOnce(false)
+            const criarClienteDto = builder.build()
+
+            expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Não foi possível salvar no banco de dados'))
+        })
+
     })
 
     describe('Validacao do cliente', () => {
         describe('Nome', () => {
             test('Deve lançar um erro quando o nome do cliente tem menos de 3 caracteres', () => {
-                const criarClienteDto = {
-                    nome: 'ze',
-                    idade: 20,
-                    email: 'teste@email.com'
-                }
+                const criarClienteDto = builder.comNome('ze').build()
 
                 expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Nome deve possuir no mínimo 3 letras'))
             })
 
             test('Deve lançar um erro quando o nome do cliente esta vazio', () => {
-                const criarClienteDto = {
-                    nome: '',
-                    idade: 20,
-                    email: 'teste@email.com'
-                }
+                const criarClienteDto = builder.comNome('').build()
+
 
                 expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Nome deve possuir no mínimo 3 letras'))
             })
 
-            test('Deve lançar um erro quando o nome do cliente tem mais de 50 caracteres', async () => {
-                const criarClienteDto = {
-                    nome: new Array(51).fill('a').join(''),
-                    idade: 20,
-                    email: 'teste@email.com'
-                }
+            test('Deve lançar um erro quando o nome do cliente tem mais de 50 caracteres', () => {
+                const criarClienteDto = builder.comNome(new Array(51).fill('a').join('')).build()
 
                 expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Nome deve possuir no máximo 50 letras'))
 
             })
 
             test('Deve lançar um erro quando o nome do cliente tem mais 3 caracteres, mas tem menos ao excluir espaços vazios', async () => {
-                const criarClienteDto = {
-                    nome: 'ze   ',
-                    idade: 20,
-                    email: 'teste@email.com'
-                }
+                const criarClienteDto = builder.comNome('ze     ').build()
+
                 expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Nome deve possuir no mínimo 3 letras'))
             })
 
             test('Deve salvar o cliente quando seu nome tem 50 caracteres', async () => {
-                const criarClienteDto = {
-                    nome: new Array(5).fill('a').join(''),
-                    idade: 20,
-                    email: 'teste@email.com'
-                }
+                const criarClienteDto = builder.comNome(new Array(50).fill('a').join('')).build()
 
                 await clienteService.criarCliente(criarClienteDto)
 
                 expect(salvarCliente).toHaveBeenCalled()
             })
 
-            test('Deve salvar o cliente quando seu nome tem 4 caracteres', async () => {
-                const criarClienteDto = {
-                    nome: 'Teo',
-                    idade: 20,
-                    email: 'teste@email.com'
-                }
+            test('Deve salvar o cliente quando seu nome tem 3 caracteres', async () => {
+                const criarClienteDto = builder.comNome('Teo').build()
 
                 await clienteService.criarCliente(criarClienteDto)
 
@@ -80,41 +73,28 @@ describe('ClienteService', () => {
 
         describe('Email', () => {
             test('Deve lançar um erro quando o email do cliente nao tem @', () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: 20,
-                    email: 'teste.email.com'
-                }
+                const criarClienteDto = builder.comEmail('email.email.com').build()
+
 
                 expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Formato de email inválido'))
             })
 
             test('Deve lançar um erro quando o email do cliente nao tem dominio', () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: 20,
-                    email: 'teste@'
-                }
+                const criarClienteDto = builder.comEmail('email@').build()
+
 
                 expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Formato de email inválido'))
             })
 
             test('Deve lançar um erro quando o email do cliente nao tem endereco', () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: 20,
-                    email: '@gmail.com'
-                }
+                const criarClienteDto = builder.comEmail('@gmail.com').build()
+
 
                 expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Formato de email inválido'))
             })
 
             test('Deve salvar o cliente quando o email for valido', async () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: 20,
-                    email: 'teste@gmail.com'
-                }
+                const criarClienteDto = builder.comEmail('email@email.com').build()
 
                 await clienteService.criarCliente(criarClienteDto)
 
@@ -124,128 +104,66 @@ describe('ClienteService', () => {
 
         describe('Idade', () => {
             test('Deve lançar um erro quando o cliente tem menos de 18 anos', () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: 17,
-                    email: 'teste@email.com'
-                }
+                const criarClienteDto = builder.comIdade(17).build()
 
                 expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Você deve possuir mais de 18 anos para se cadastrar'))
             })
 
             test('Deve lançar um erro quando o cliente possui uma idade que nao seja um numero inteiro', () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: 21.3,
-                    email: 'teste@email.com'
-                }
+                const criarClienteDto = builder.comIdade(21.3).build()
+
 
                 expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Valor informado para idade é inválido.'))
             })
 
             test('Deve lançar um erro quando o cliente possui uma idade negativa', () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: -1,
-                    email: 'teste@email.com'
-                }
+                const criarClienteDto = builder.comIdade(-1).build()
+
 
                 expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Você deve possuir mais de 18 anos para se cadastrar'))
             })
 
             test('Deve lançar um erro quando o cliente possui mais de 110 anos', () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: 111,
-                    email: 'teste@email.com'
-                }
+                const criarClienteDto = builder.comIdade(111).build()
+
 
                 expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Você deve possuir menos de 110 anos para se cadastrar'))
             })
 
 
             test('Deve salvar o cliente quando possuir 18 anos', async () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: 18,
-                    email: 'teste@email.com'
-                }
+                const criarClienteDto = builder.comIdade(18).build()
+
                 await clienteService.criarCliente(criarClienteDto)
 
                 expect(salvarCliente).toHaveBeenCalled()
             })
 
             test('Deve salvar o cliente quando possuir 110 anos', async () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: 110,
-                    email: 'teste@email.com'
-                }
+                const criarClienteDto = builder.comIdade(110).build()
+
                 await clienteService.criarCliente(criarClienteDto)
 
                 expect(salvarCliente).toHaveBeenCalled()
             })
         })
 
-        describe('Idade', () => {
-            test('Deve lançar um erro quando o cliente tem menos de 18 anos', () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: 17,
-                    email: 'teste@email.com'
-                }
+        describe('Telefone', () => {
+            test('Deve lançar um erro quando o telefone não possuir ddd', () => {
+                const criarClienteDto = builder.comTelefone(912345678).build()
 
-                expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Você deve possuir mais de 18 anos para se cadastrar'))
+                expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Telefone inválido. O telefone precisa ser um celular com ddd. ex: 32988888888'))
             })
 
-            test('Deve lançar um erro quando o cliente possui uma idade que nao seja um numero inteiro', () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: 21.3,
-                    email: 'teste@email.com'
-                }
+            test('Deve lançar um erro quando o telefone não possuir o digito 9', () => {
+                const criarClienteDto = builder.comTelefone(1112345678).build()
 
-                expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Valor informado para idade é inválido.'))
+                expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Telefone inválido. O telefone precisa ser um celular com ddd. ex: 32988888888'))
             })
 
-            test('Deve lançar um erro quando o cliente possui uma idade negativa', () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: -1,
-                    email: 'teste@email.com'
-                }
+            test('Deve salvar o cliente quando o telefone possuir ddd, 9 e o numero', async () => {
+                const criarClienteDto = builder.comTelefone(11912345678).build()
 
-                expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Você deve possuir mais de 18 anos para se cadastrar'))
-            })
-
-            test('Deve lançar um erro quando o cliente possui mais de 110 anos', () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: 111,
-                    email: 'teste@email.com'
-                }
-
-                expect(async () => await clienteService.criarCliente(criarClienteDto)).rejects.toThrow(new Error('Você deve possuir menos de 110 anos para se cadastrar'))
-            })
-
-
-            test('Deve salvar o cliente quando possuir 18 anos', async () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: 18,
-                    email: 'teste@email.com'
-                }
-                await clienteService.criarCliente(criarClienteDto)
-
-                expect(salvarCliente).toHaveBeenCalled()
-            })
-
-            test('Deve salvar o cliente quando possuir 110 anos', async () => {
-                const criarClienteDto = {
-                    nome: 'Nome Valido',
-                    idade: 110,
-                    email: 'teste@email.com'
-                }
                 await clienteService.criarCliente(criarClienteDto)
 
                 expect(salvarCliente).toHaveBeenCalled()
@@ -255,11 +173,7 @@ describe('ClienteService', () => {
 
     describe('Criacao de id', () => {
         test('Deve criar um id do tipo uuidV4 para o cliente', async () => {
-            const criarClienteDto = {
-                nome: 'NomeValido',
-                idade: 20,
-                email: 'emailValido@email.com'
-            }
+            const criarClienteDto = builder.build()
 
             const cliente = await clienteService.criarCliente(criarClienteDto)
 
@@ -267,5 +181,8 @@ describe('ClienteService', () => {
             expect(version(cliente.id)).toEqual(4)
         })
     })
+
 })
+
+
 
